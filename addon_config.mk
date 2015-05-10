f2b380eb83d0a847cc88f61be70673f12bdd1363
Andrew McWilliams <andy@jahya.net>
2015-05-10 13:14:25 -0400

    Adds config file

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxLayerMask
+	ADDON_DESCRIPTION = Use an alpha mask to reveal a foreground layer against a background
+	ADDON_AUTHOR = Andrew McWilliams
+	ADDON_TAGS = alpha mask layer reveal
+	ADDON_URL = http://github.com/microcosm/ofxLayerMask
